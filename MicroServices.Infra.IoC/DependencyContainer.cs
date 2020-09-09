deee67b00ecdfbe6defee050fcefb8b4b3ae2e34
white-devil-lord <vm2508.cs@gmail.com>
2020-09-09 13:22:29 +0530

    Initial Commit of Infrastructure Dependency Project

---
+++ MicroServices.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroServices.Domain.Core.Bus;
+using MicroServices.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.Infra.IoC
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			services.AddTransient<IEventBus, RabbitMQBus>();
+
+		}
+	}
+}
