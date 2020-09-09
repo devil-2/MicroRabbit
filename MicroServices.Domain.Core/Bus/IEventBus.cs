60f1c4b4e54b259242864eb84014f9beb10cf636
white-devil-lord <vm2508.cs@gmail.com>
2020-09-09 10:51:12 +0530

    Implemented Interfaces and classes for Domain

---
+++ MicroServices.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using MicroServices.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroServices.Domain.Core.Events;
+
+namespace MicroServices.Domain.Core.Bus
+{
+ 	public interface IEventBus
+	{
+		Task SendCommand<T>(T command) where T:Command;
+
+		void Publish<T>(T @event) where T : Event;
+
+		void Subscribe<T, TH>()
+			where T : Event
+			where TH : IEventHandler<T>;
+	}
+}
---
+++ MicroServices.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using MicroServices.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.Domain.Core.Commands
+{
+	public abstract class Command:Message
+	{
+		public DateTime TimeStamp { get; protected set; }
+
+		protected Command()
+		{
+			TimeStamp = DateTime.Now;
+		}
+	}
+}
