fc810d95a79fb42b8c1975cdfda22eeea1c52939
white-devil-lord <vm2508.cs@gmail.com>
2020-09-11 20:39:33 +0530

    Completed BankingMicroServices RabbitMQ

---
+++ MicroServices.MVC/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MicroServices.MVC.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServices.MVC.Services
+{
+	public interface ITransferService
+	{
+		Task Transfer(TransferDto transferDto);
+	}
+}
